42433ea3709848f05fd69bd3b1c6e373e7552e1b
zhaojl@hshbao.com <zhaojl@hshbao.com>
2019-08-07 16:01:46 +0800

    fix

--- msg/src/main/java/org/cool/msg/MsgAutoConfiguration.java
+++ msg/src/main/java/org/cool/msg/MsgAutoConfiguration.java
@@ -1,14 +1,12 @@
 package org.cool.msg;
 
 import org.cool.msg.interceptor.MsgPreValidatorHandlerInterceptor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(value = "msg")
 @ComponentScan({"org.cool.msg.*"})
 public class MsgAutoConfiguration {
 
