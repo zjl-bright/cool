7cd7554e637258d7fa7a1bc155630cf2fce7dfc7
zhaojl@hshbao.com <zhaojl@hshbao.com>
2019-10-14 15:01:56 +0800

    fix

--- cool-spring/mybatisplus/src/main/java/org/cool/mybatisplus/entity/BaseEntity.java
+++ cool-spring/mybatisplus/src/main/java/org/cool/mybatisplus/entity/BaseEntity.java
@@ -1,31 +1,39 @@
 /*
  * Copyright (c) 2019~2021. zjl. All rights reserved.
  */
-//package org.cool.common.model;
-//
-//import com.baomidou.mybatisplus.annotation.FieldFill;
-//import com.baomidou.mybatisplus.annotation.IdType;
-//import com.baomidou.mybatisplus.annotation.TableField;
-//import com.baomidou.mybatisplus.annotation.TableId;
-//import com.hshbao.common.api.model.BaseUser;
-//import lombok.Data;
-//import lombok.experimental.Accessors;
-//
-//import java.time.LocalDateTime;
+package org.cool.mybatisplus.entity;
 
-//@Data
-//@Accessors(chain = true)
-//public class BaseEntity implements BaseUser {
-//
-//    @TableId(value = "id", type = IdType.AUTO)
-//    private Long id;
-//
-//    @TableField(value = "status", fill = FieldFill.INSERT)
-//    private Integer status;
-//
-//    @TableField(value = "created_at", fill = FieldFill.INSERT)
-//    private LocalDateTime createdAt;
-//
-//    @TableField(value = "updated_at", fill = FieldFill.INSERT_UPDATE)
-//    private LocalDateTime updatedAt;
-//}
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.cool.common.model.BaseUser;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Auther: zhaojl@hshbao.com
+ * @Date: 2018/12/18
+ * @Description:
+ * @Version: 1.0
+ */
+@Getter
+@Setter
+@Accessors(chain = true)
+public class BaseEntity implements BaseUser {
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @TableField(value = "status", fill = FieldFill.INSERT)
+    private Integer status;
+
+    @TableField(value = "created_at", fill = FieldFill.INSERT)
+    private LocalDateTime createdAt;
+
+    @TableField(value = "updated_at", fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updatedAt;
+}
--- cool-spring/mybatisplus/src/main/java/org/cool/mybatisplus/handler/MyMetaObjectHandler.java
+++ cool-spring/mybatisplus/src/main/java/org/cool/mybatisplus/handler/MyMetaObjectHandler.java
@@ -1,29 +1,28 @@
 /*
  * Copyright (c) 2019~2021. zjl. All rights reserved.
  */
-//package org.cool.common.handler;
-//
-//import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
-//import org.apache.ibatis.reflection.MetaObject;
-//import org.springframework.stereotype.Component;
-//
-//import java.time.LocalDateTime;
+package org.cool.mybatisplus.handler;
 
-//@Component
-//public class MyMetaObjectHandler implements MetaObjectHandler {
-//
-//    @Override
-//    public void insertFill(MetaObject metaObject) {
-//        //新增填充
-//        setFieldValByName("status", 1, metaObject);
-//        setFieldValByName("createdAt", LocalDateTime.now(), metaObject);
-//        setFieldValByName("updatedAt", LocalDateTime.now(), metaObject);
-//    }
-//
-//    @Override
-//    public void updateFill(MetaObject metaObject) {
-//        //更新填充
-//        setFieldValByName("updatedAt", LocalDateTime.now(), metaObject);
-//    }
-//}
-//
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        //新增填充
+        setFieldValByName("status", 1, metaObject);
+        setFieldValByName("createdAt", LocalDateTime.now(), metaObject);
+        setFieldValByName("updatedAt", LocalDateTime.now(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        //更新填充
+        setFieldValByName("updatedAt", LocalDateTime.now(), metaObject);
+    }
+}
