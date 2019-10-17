dd5bdd9490c72be5a74ee8b82a33407c8da94861
zhaojl@hshbao.com <zhaojl@hshbao.com>
2019-10-17 10:45:28 +0800

    fix

--- common/src/main/java/org/cool/common/enums/Status.java
+++ common/src/main/java/org/cool/common/enums/Status.java
@@ -15,9 +15,9 @@ public enum Status {
 
     DELETED("已删除", -1),
 
-    DISABLED("已禁用，未激活，无效，不是", 0),
+    DISABLED("已禁用，未激活，无效，无, 不是, 无效, 不共享", 0),
 
-    ENABLED("已启用, 已激活, 有效, 是", 1);
+    ENABLED("已启用, 已激活, 有效, 有, 是, 有效, 共享", 1);
 
     private String describe;
 
